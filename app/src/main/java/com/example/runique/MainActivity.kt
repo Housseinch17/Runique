b7fb4d2ab31fbe08ff3f45c468a21c9705717a7b
HousseinCh <houssein.ch@live.com>
2025-12-17 15:40:09 +0200

    :lipstick: Create Material Theme

--- app/src/main/java/com/example/runique/MainActivity.kt
+++ app/src/main/java/com/example/runique/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.runique.ui.theme.RuniqueTheme
+import com.example.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             RuniqueTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Box(modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        contentAlignment = Alignment.Center){
+                        Text(
+                            modifier = Modifier,
+                            text = "This is MainActivity"
+                        )
+                    }
                 }
             }
         }
--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -14,11 +14,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.material3)
-    debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(platform(libs.androidx.compose.bom))
 
     implementation(libs.google.android.gms.play.services.location)
